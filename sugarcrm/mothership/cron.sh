fa934e2142373b6f61b430ec4f04270138bbe6e2
Stanislav Malyshev <smalyshev@gmail.com>
2013-01-21 13:41:09 -0800

    add mothership scripts

---
+++ sugarcrm/mothership/cron.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Run: cron.sh servername template
+# Example: cron.sh foobarbaz.msdev.sugarcrm.com /mnt/sugar/6.6.3/ent
+export SERVER_NAME=$1
+export REMOTE_ADDR=$1
+export DOCUMENT_ROOT="$2"
+export SHADOW_ROOT="$2"
+cd /mnt/sugar/shadowed/$SERVER_NAME && php -dauto_prepend_file="/mnt/sugar/SugarShadowEnv.php" -f $DOCUMENT_ROOT/cron.php
---
+++ sugarcrm/mothership/phpunit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Run: phpunit.sh servername template other-args
+# Example: phpunit.sh foobarbaz.msdev.sugarcrm.com /mnt/sugar/6.6.3/ent --log-junit "/mnt/sugar/shadowed/foobarbaz.msdev.sugarcrm.com/phpunit.xml"
+export SERVER_NAME=$1
+export REMOTE_ADDR=$1
+export DOCUMENT_ROOT="$2"
+export SHADOW_ROOT="$2"
+cd /mnt/sugar/shadowed/$SERVER_NAME/tests && php -dauto_prepend_file="/mnt/sugar/SugarShadowEnv.php" phpunit.php $*
