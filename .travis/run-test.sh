63b75460afcf41d70c25adc35ee978743f142132
Derrik Pates <demon@now.ai>
2016-07-29 11:50:35 -0600

    [OPS-4295] Add the bits that should turn on travis-ci testing.

---
+++ .travis/run-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export TEST_PHP_EXECUTABLE=`which php`
+TEST_DIR="`pwd`/tests"
+
+php_zts=$(php -r "echo PHP_ZTS;")
+
+if [ $php_zts -eq 1 ] ; then
+    echo "Skipping tests for ZTS, because we currently don't work with ZTS PHP"
+    exit 0
+else
+    cd $(pwd)
+    make test
+fi
