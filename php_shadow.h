13ae6c969a033e0506ec3779d3b3f7017dfd6a0e
Jason Corley <jcorley@sugarcrm.com>
2016-10-05 13:58:14 -0400

    [BR-4592] increment version now that we've added support for new versions of PHP

--- php_shadow.h
+++ php_shadow.h
@@ -69,7 +69,7 @@ ZEND_END_MODULE_GLOBALS(shadow)
 #define SHADOW_G(v) (shadow_globals.v)
 #endif
 
-#define SHADOW_VERSION "0.3.12"
+#define SHADOW_VERSION "0.4.0"
 
 ZEND_EXTERN_MODULE_GLOBALS(shadow)
 
