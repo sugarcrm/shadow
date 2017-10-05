77aa1b2b12bf47cbec2e2dbb60c2d01296143b2b
S. Zachariah Sprackett <zac@sprackett.com>
2017-10-04 23:45:58 -0400

    [BR-5535] bump version to 0.8.0

--- php_shadow.h
+++ php_shadow.h
@@ -68,7 +68,7 @@ ZEND_END_MODULE_GLOBALS(shadow)
 #define SHADOW_G(v) (shadow_globals.v)
 #endif
 
-#define SHADOW_VERSION "0.7.0"
+#define SHADOW_VERSION "0.8.0"
 
 ZEND_EXTERN_MODULE_GLOBALS(shadow)
 
